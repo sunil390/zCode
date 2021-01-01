f1aa74ef3cfbbbe7c8e00a8ae7d5f28bae551e91
sunil390 <75531992+sunil390@users.noreply.github.com>
2021-01-01 23:51:31 +0530

    csv file creation

--- FileGenerator.py
+++ FileGenerator.py
@@ -1,7 +1,6 @@
-# SKS 12/31/2020
-# SKS 01/01/2021
+# SKS 12/31/2020 Created output in multiple files
+# SKS 01/01/2021 Created a single csv file
 # https://stackoverflow.com/questions/7152762/how-to-redirect-print-output-to-a-file-using-python 
-# Since Python 3.4 there's a simple context manager available to redirect print stdout to file in standard library:
 from contextlib import redirect_stdout
 # https://www.w3schools.com/python/python_lists.asp
 CellList1 = ['h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','aa','ab','ac','ad','ae','af','ag','ah','ai','aj','ak','al']
@@ -10,29 +9,10 @@ CellList2 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','
 i = 6
 while i < 76:
 # https://www.w3schools.com/python/python_for_loops.asp
-    for x in CellList1:
-# https://stackoverflow.com/questions/7152762/how-to-redirect-print-output-to-a-file-using-python
-        with open('FShiftCode.txt', 'a') as f:
-            with redirect_stdout(f):
+    for x in CellList1 :
+        for y in CellList2 :
+            with open('ShiftLoad.csv', 'a') as f:
+                with redirect_stdout(f):
 # https://www.geeksforgeeks.org/python-sep-parameter-print/ removing spaces between arguments.
-                print('=EmployeeShiftImport.xlsx!',x,i, sep='')
-        with open('FOnCallCode.txt', 'a') as f:
-            with redirect_stdout(f):
-                print('=Oncall!',x,i, sep='')
-        with open('FMember.txt', 'a') as f:
-            with redirect_stdout(f):
-                print('=Oncall!F',i, sep='')
-        with open('FEmail.txt', 'a') as f:
-            with redirect_stdout(f):
-                print('=Oncall!D',i, sep='')
-        with open('FGroup.txt', 'a') as f:
-            with redirect_stdout(f):
-                print('=Oncall!B',i, sep='')
-    i += 1
-i = 6
-while i < 76:
-    for y in CellList2:
-        with open('FShiftDate.txt', 'a') as f:
-            with redirect_stdout(f):
-                print(y,'/01/2021', sep='')
+                    print('=EmployeeShiftImport.xlsx!',x,i,',','=Oncall!',x,i,',','=Oncall!F',i,',','=Oncall!D',i,',','=Oncall!B',i,',',y,'/01/2021', sep='')
     i += 1
